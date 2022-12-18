96ac2f03fe0fe4aee0c22c533bc025c622322f35
Ismael Real Portillo <ireal-po@c1r15s6.42malaga.com>
2022-12-18 20:24:01 +0100

    Subo función strrchr
    Es igual que la strchr, pero en ésta buca el carácter c en
    el string

---
+++ ft_strrchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ireal-po <ireal-po@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 19:08:30 by ireal-po          #+#    #+#             */
+/*   Updated: 2022/12/18 20:14:32 by ireal-po         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int		lastc;
+	int		i;
+	char	*scpy;
+
+	i = 0;
+	lastc = -1;
+	scpy = (char *)s;
+	if ((char)c == '\0')
+		return (&(scpy[ft_strlen(scpy)]));
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			lastc = i;
+		i++;
+	}
+	if (lastc == -1)
+		return (NULL);
+	return (&scpy[lastc]);
+}
+
+// int	main(void)
+// {
+// 	char	s[] = "Hola Paco como estas";
+// 	int		a;
+// 	a = 'P';
+// 	printf("%c\n", (char)a);
+// 	printf("%s", ft_strrchr(s, (int)a));
+// 	return(0);
+// }
